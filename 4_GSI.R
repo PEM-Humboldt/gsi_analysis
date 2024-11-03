da942cafd4087e2ab0aa3bc75d3182c53a8eed80
Elkin01 <ek.tenorio@gmail.com>
2024-11-03 16:03:25 -0500

    Sf/terra update

--- 4_GSI.R
+++ 4_GSI.R
@@ -1,78 +1,96 @@
 
-library(raster)
-library(rgdal)
+library(terra)
+library(sf)
 library(hyperSpec)
+library(RColorBrewer)
+
 # ---------------------------
 #   Data loading
 # ---------------------------
-wd <- 'F:/GSI/'
-setwd(wd)
-dir.create("GSI/")
+# Establecer el directorio de trabajo y crear un directorio para resultados
+wd <- ''
+setwd(wd)                              # Cambiar el directorio de trabajo a la ruta especificada
+dir.create("GSI/")                     # Crear un nuevo directorio para guardar los resultados
 
+# Definir el sistema de referencia de coordenadas (CRS)
+GRS.geo <- '+proj=longlat +datum=WGS84 +no_defs'
 
-GRS.geo<- '+proj=longlat +datum=WGS84 +no_defs'
-col<-getData(name = 'GADM', country = 'COL', level = 0) #Areo of interes (AOI)
-col@data$OBJECTID<-1
-colr<-raster(extent(col), res=0.008333333)
-colrast<- rasterize(col,colr)
+# Leer el shapefile del área de interés (AOI)
+###col <- geodata::gadm(country= "COL", level=0, path= ".") #Areo of interes (AOI) # función desactivada. Sirve para cargar shapefile de Colombia desde servidor remoto del paquete geodata
+col <- read_sf('/Carpeta_shapefile', 'shapefile')
+col <- sf::st_transform(col, GRS.geo)  # Transformar el CRS del shapefile
+colrast <- rast(col, res=0.008333333)  # Crear un raster con la resolución especificada
+colrast <- terra::rasterize(col, colrast, fun="sum") # Rasterizar el shapefile
 
 # ---------------------------
 # 4. GSI / Gap selection index
 # ---------------------------
 
-# Estandarizacion de valores
-
-#densRegistros <- raster('INDICE_DENSIDAD.tif')
-#d <- raster('INDICE_AMBIENTAL.tif')
-DENSIDAD <- raster("1_Records/Vac_dens_rescal_1km.tif")
-crs(DENSIDAD)<-GRS.geo
-AMBIENTAL <- raster("2_Ambiental/ambiental_dimension_GSI_rescal.tif")
-crs(AMBIENTAL)<-GRS.geo
-
-COMPLEMENTARIEDAD_BOOT<-raster("3_Complementarity/Complementariedad_Bootstrap.tif")
-crs(COMPLEMENTARIEDAD_BOOT)<-GRS.geo
-COMPLEMENTARIEDAD_JACK<-raster("3_Complementarity/Complementariedad_Jacknife.tif")
-crs(COMPLEMENTARIEDAD_JACK)<-GRS.geo
+# Estandarización de valores
 
 
+# Leer los rasters de datos
+DENSIDAD <- rast("1_Records/Vac_dens_rescal_1km.tif") # Raster de densidad de registros
+crs(DENSIDAD) <- GRS.geo                      # Asignar el CRS al raster de densidad
+AMBIENTAL <- rast("2_Ambiental/ambiental_dimension_GSI_rescal_ajusvar.tif") # Raster ambiental
+crs(AMBIENTAL) <- GRS.geo                    # Asignar el CRS al raster ambiental
+
+# Leer los rasters de complementariedad
+COMPLEMENTARIEDAD_BOOT <- rast("3_Complementarity/Complementariedad_Bootstrap.tif")
+crs(COMPLEMENTARIEDAD_BOOT) <- GRS.geo       # Asignar el CRS al raster de bootstrap
+COMPLEMENTARIEDAD_JACK <- rast("3_Complementarity/Complementariedad_Jacknife.tif")
+crs(COMPLEMENTARIEDAD_JACK) <- GRS.geo        # Asignar el CRS al raster de jackknife
+
+# Reescalar rasters a la misma resolución que el raster de densidad
 ambiental_resample <-resample(AMBIENTAL, DENSIDAD)
 comp_jack <-resample(COMPLEMENTARIEDAD_JACK, DENSIDAD)
 comp_boot <-resample(COMPLEMENTARIEDAD_BOOT, DENSIDAD)
 
+# Filtrar valores no NA y no cero del raster ambiental
 AMBValsNoNA <- AMBIENTAL[!is.na(AMBIENTAL[])]
 AMBValsNoNANoZeros <- AMBIENTAL[!is.na(AMBIENTAL[]) & AMBIENTAL[] != 0]
 hist(AMBValsNoNA, main = 'Ambiental no NA')
 hist(AMBValsNoNANoZeros, main = 'Ambiental /n no NA no 0')
 
-# # Completness
+# Convertir el objeto 'col' a tipo 'vect' para su uso en gráficos
+col=vect(col)
+
+# Graficar las capas de complementariedad
 par(mfrow = c(1, 2))
 plot(COMPLEMENTARIEDAD_BOOT, main = 'Bootstrap')
-plot(col, add = TRUE)
+lines(col, lwd=1)
 plot(COMPLEMENTARIEDAD_JACK, main = 'Jackknife')
-plot(col, add = TRUE)
+lines(col, lwd=1)
 
-#  GSI / GAP INDEX
-rasterOptions(tolerance = 0.1)
-GSI_BOOT <- (3- DENSIDAD - ambiental_resample- comp_boot)/3
-GSI_JACK <- (3-DENSIDAD - ambiental_resample - comp_jack)/3
+# Calcular el índice de selección de huecos (GSI) para ambos métodos
+terraOptions(tolerance = 0.1) # Ajustar opciones de tolerancia
+GSI_BOOT <- (3 - DENSIDAD - ambiental_resample - comp_boot) / 3 # Índice GSI para bootstrap
+GSI_JACK <- (3 - DENSIDAD - ambiental_resample - comp_jack) / 3 # Índice GSI para jackknife
 
+
+# Definir una paleta de colores y establecer el rango de valores para la visualización
+color_palette <- colorRampPalette(brewer.pal(9, "YlGnBu"))(100)
+zlim <- c(0, 1)
+
+# Graficar los índices GSI
 par(mfrow = c(1, 2))
+plot(GSI_BOOT, main = 'GAP SELCTION INDEX (BOOT)', range = zlim)
+lines(col, lwd=1)
+plot(GSI_JACK, main = 'GAP SELCTION INDEX (JACK)', range = zlim)
+lines(col, lwd=1)
 
-plot(GSI_BOOT, main = 'GAP SELCTION INDEX (BOOT)', zlim = c(0, 1))
-plot(col, add = TRUE)
+# Graficar densidades de los índices GSI
+par(mfrow = c(1, 2))
+density(GSI_JACK, main = 'Jackk', xlim = c(0, 1))
+density(GSI_BOOT, main = 'Boot', xlim = c(0, 1))
 
-plot(GSI_JACK, main = 'GAP SELCTION INDEX (JACK)', zlim = c(0, 1))
-plot(col, add = TRUE)
+#par(mfrow = c(1, 1))
+#plot(densJack, main = 'GSI values', col = 'blue', ylim = c(0, 10))
+#lines(densBoot, col = 'red')
+#legend('topleft', legend = c('Jackknife', 'Bootstrap'), 
+#       lty = c(1, 1), lwd = c(1, 1), col = c('blue', 2))
 
-densJack <- density(GSI_JACK, main = 'Jackk', xlim = c(0, 1))
-densBoot <- density(GSI_BOOT, main = 'Boot', xlim = c(0, 1))
-
-par(mfrow = c(1, 1))
-plot(densJack, main = 'GSI values', col = 'blue', ylim = c(0, 10))
-lines(densBoot, col = 'red')
-legend('topleft', legend = c('Jackknife', 'Bootstrap'), 
-       lty = c(1, 1), lwd = c(1, 1), col = c('blue', 2))
-
-
+# Guardar los resultados en archivos raster
 writeRaster(GSI_BOOT,"GSI/INDICE_GSI_BOOT.tif", overwrite = TRUE)
 writeRaster(GSI_JACK,"GSI/INDICE_GSI_JACK.tif", overwrite = TRUE)
+
